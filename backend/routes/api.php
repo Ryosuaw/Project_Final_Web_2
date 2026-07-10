2a76f1a5a688c53b3ab2a40ab1d7ed22fe75fdbd
lilis puspitasari968 <lilispuspitasari968.com>
2026-07-10 15:55:01 +0800

    Tambah ulang routes api.php yang sempat hilang

---
+++ backend/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SkillExchangeController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test-data', function () {
+    return response()->json([
+        'pesan' => 'Halo! Ini data uji coba dari backend Laravel',
+        'status' => 'Sukses Terhubung'
+    ]);
+});
+
+Route::get('/skill-exchange', [SkillExchangeController::class, 'index']);
+Route::post('/skill-exchange/{userId}/request', [SkillExchangeController::class, 'sendRequest']);
+Route::post('/skill-exchange/requests/{requestId}/accept', [SkillExchangeController::class, 'acceptRequest']);
+Route::post('/skill-exchange/requests/{requestId}/reject', [SkillExchangeController::class, 'rejectRequest']);
